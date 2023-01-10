b1b0ee5c64b617a3e99c662cfec4efc8d4213f69
unknown <davidli4532@gmail.com>
2023-01-10 19:40:11 +0100

    update siteweb

--- ajouterscan.php
+++ ajouterscan.php
@@ -1,70 +1,69 @@
 <?php include("headeradminscan.php") ?>
 
 
-
-    <div class="container">
-      <form method="POST" action="ajouter_formscan.php" enctype="multipart/form-data">
-        <div class="row">
-          <div class="col-25">
-            <label for="Prénom">Nom de l'anime</label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="nom" placeholder="Nom de l'anime">
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-25">
-            <label for="Prix">la note</label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="note" placeholder="Saisir la note (A-E)">
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-25">
-            <label for="Prix">Nombre d'épisodes </label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="eprn" placeholder="Sasir le nombre d'épisode actuallement">
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-25">
-            <label for="Prix">L'anime est-il finis ou pas </label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="yn" placeholder="Saisir O ou N">
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-25">
-            <label for="Prix">le lien du site pour visionner </label>
-          </div>
-          <div class="col-75">
-            <input type="text" name="lien" placeholder="Saisir le lien du site pour visualiser">
-          </div>
-        </div>
-
-
-
-        <div class="row">
-          <div class="col-25">
-            <label for="Sujet">Images</label>
-          </div>
-          <div class="col-75">
-            <input type="file" name="photo" placeholder="Selectionnez l'image" ></textarea>
-          </div>
-        </div>
-
-        <div class="row">
-          <input type="submit" value="Envoyer">
-        </div>
-      </form>
+<br><br>
+<div class="container">
+  <form method="POST" action="ajouter_formscan.php" enctype="multipart/form-data">
+    <div class="row">
+      <div class="col-25">
+        <label for="Prénom" style="color:pink;">Nom de l'anime</label>
+      </div>
+      <div class="col-75">
+        <input type="text" name="nom" placeholder="Nom de l'anime">
+      </div>
     </div>
 
-    <?php include("footer.php") ?>
+    <div class="row">
+      <div class="col-25">
+        <label for="Prix" style="color:pink;">la note</label>
+      </div>
+      <div class="col-75">
+        <input type="text" name="note" placeholder="Saisir la note (A-E)">
+      </div>
+    </div>
 
+    <div class="row">
+      <div class="col-25">
+        <label for="Prix" style="color:pink;">Nombre d'épisodes </label>
+      </div>
+      <div class="col-75">
+        <input type="text" name="eprn" placeholder="Sasir le nombre d'épisode actuallement">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-25">
+        <label for="Prix" style="color:pink;">L'anime est-il finis ou pas </label>
+      </div>
+      <div class="col-75">
+        <input type="text" name="yn" placeholder="Saisir O ou N">
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-25">
+        <label for="Prix" style="color:pink;">le lien du site pour visionner </label>
+      </div>
+      <div class="col-75">
+        <input type="text" name="lien" placeholder="Saisir le lien du site pour visualiser">
+      </div>
+    </div>
+
+
+
+    <div class="row">
+      <div class="col-25">
+        <label for="Sujet" style="color:pink;">Images</label>
+      </div>
+      <div class="col-75">
+        <input type="file" name="photo" placeholder="Selectionnez l'image"></textarea>
+      </div>
+    </div>
+    <br><br>
+    <div class="row">
+      <input type="submit" value="Envoyer">
+    </div>
+  </form>
+</div>
+
+<?php include("footer.php") ?>
--- headerscan.php
+++ headerscan.php
@@ -1,46 +1,47 @@
 <?php
-session_start() ;
+session_start();
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  </head>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
 
 
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="scan.php"><img src="Images/logo.jpg" height="100" width="100" alt="..."></a>
-        <div class="btn-group">
-          <a href="index.php" class="btn btn-secondary" aria-current="page">Page d'Anime</a>
-        </div>
-        
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-          <div class="container-fluid">
-            <form class="d-flex" method="POST" action='recherchevisiscan.php'>
-              <input class="form-control me-2" type="search" placeholder="Rechercher des scans" aria-label="Recherche" name="recherche">
-              <input class="btn btn-outline-success" type="submit" value="Rechercher" >
-            </form>
-          </div>
-        </div>
-        <div class="btn-group">
-        <a href="indexascan.php" class="btn btn-outline-secondary" aria-current="page">A</a>
-          <a href="indexbscan.php" class="btn btn-outline-secondary" aria-current="page">B</a>
-          <a href="indexcscan.php" class="btn btn-outline-secondary" aria-current="page">C</a>
-          <a href="indexdscan.php" class="btn btn-outline-secondary" aria-current="page">D</a>
-          <a href="indexescan.php" class="btn btn-outline-secondary" aria-current="page">E</a>
-          </div>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php"><img src="Images/logo.jpg" height="100" width="100" alt="..."></a>
+      <div class="btn-group">
+        <a href="index.php" class="btn btn-secondary" aria-current="page">Page d'Anime</a>
       </div>
-      <!-- Button trigger modal -->
-<a href="indexadminscan.php" type="button" class="btn btn-danger">
-Administration
-</a>
 
-<!-- Modal -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+        <div class="container-fluid">
+          <form class="d-flex" method="POST" action='recherchevisiscan.php'>
+            <input class="form-control me-2" type="search" placeholder="Rechercher des scans" aria-label="Recherche" name="recherche">
+            <input class="btn btn-outline-success" type="submit" value="Rechercher">
+          </form>
+        </div>
+      </div>
+      <div class="btn-group">
+        <a href="indexascan.php" class="btn btn-outline-secondary" aria-current="page">A</a>
+        <a href="indexbscan.php" class="btn btn-outline-secondary" aria-current="page">B</a>
+        <a href="indexcscan.php" class="btn btn-outline-secondary" aria-current="page">C</a>
+        <a href="indexdscan.php" class="btn btn-outline-secondary" aria-current="page">D</a>
+        <a href="indexescan.php" class="btn btn-outline-secondary" aria-current="page">E</a>
+      </div>
+    </div>
+    <!-- Button trigger modal -->
+    <a href="indexadminscan.php" type="button" class="btn btn-danger">
+      Administration
+    </a>
 
-    </nav>
+    <!-- Modal -->
+
+  </nav>
